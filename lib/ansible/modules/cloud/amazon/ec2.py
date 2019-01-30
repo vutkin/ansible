bb71f2272472b16d8ae0e68ed297cbd51bbfa92d
Viktor Utkin <viktor.utkin7@yandex.ru>
2019-01-30 15:33:43 -0500

    fix typo 2

--- lib/ansible/modules/cloud/amazon/ec2.py
+++ lib/ansible/modules/cloud/amazon/ec2.py
@@ -1278,7 +1278,7 @@ def create_instances(module, ec2, vpc, override_count=None):
                 # https://docs.aws.amazon.com/cli/latest/reference/ec2/request-spot-instances.html
                 utc_valid_until = (datetime.datetime.utcnow()
                                   + datetime.timedelta(seconds=spot_wait_timeout))
-                params['valid_until'] = utc_valid_until
+                params['valid_until'] = time.mktime(utc_valid_until)
 
                 res = ec2.request_spot_instances(spot_price, **params)
 
